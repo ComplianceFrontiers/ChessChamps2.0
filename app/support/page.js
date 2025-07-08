4b67ab7765e1637ec23d6a7b608c6ff6b2d2862f
ramya <nsriramya7@gmailcom>
2025-07-08 17:41:29 +0530

    performance added mobile view fixed

---
+++ app/support/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Support = () => {
+    return (
+        <div style={{ backgroundColor: 'white', color: 'black', minHeight: '100vh', padding: '2rem' }}>
+            <h1>support</h1>
+        </div>
+    );
+};
+
+export default Support;
